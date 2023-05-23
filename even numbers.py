3c68a2d78753fa38bed163f49259fc5ff6e54ff9
Ankitha .G.S <122465236+1ankitha@users.noreply.github.com>
2023-05-23 19:29:52 +0530

    Create even numbers.py

---
+++ even numbers.py
@@ -0,0 +1,13 @@
+n=input('enter a number:')
+if(int(n)%2)==0:
+    print("{0} the given number is a even number".format(n))
+else:
+    print("{0} the given number is odd number".format(n))
+    
+    or
+      
+n=int(input())
+if(int(n)%2)==0:
+    print("even number")
+else:
+    print("not even number")
